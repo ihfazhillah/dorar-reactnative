20d7731f77e7c6c49dc1540bf22e0109fe3b1a5e
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-07-22 12:05:40 +0700

    add simple result

--- DetailResult.js
+++ DetailResult.js
@@ -1,11 +1,27 @@
 import React, {useState, useEffect} from 'react';
-import {Text, Button, View} from 'react-native';
+import {Text, Button, View, ScrollView} from 'react-native';
 
 
 function DetailResult({navigation}){
-    return <View>
-        <Text>{navigation.getParam('q')}</Text>
-    </View>
+    const base_url = 'https://dorar.net/dorar_api.json?skey='
+
+    const [result, setResult] = useState(null)
+
+    async function fetchResult(){
+        const response = await fetch(base_url + navigation.getParam('q'))
+        const json = await response.json()
+        setResult(json)
+    }
+
+    useEffect(() => {fetchResult()}, [])
+
+    if (!result){
+        return <View><Text>Loading...</Text></View>
+    }
+
+    return <ScrollView>
+        <Text>{result.ahadith.result}</Text>
+    </ScrollView>
 }
 
 DetailResult.navigationOptions = ({navigation}) => ({
