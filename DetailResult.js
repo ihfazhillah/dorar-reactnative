a853db17f96595051b5ecb84a58c0480355b3ec4
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-08-15 11:25:19 +0700

    [master] Handle share intent text in detail page

--- DetailResult.js
+++ DetailResult.js
@@ -3,78 +3,100 @@ import {Text, Button, TouchableOpacity, View, ScrollView, FlatList} from 'react-
 import cheerio from 'cheerio-without-node-native'
 import _ from 'lodash'
 import Share from 'react-native-share';
+import ShareMenu from 'react-native-share-menu';
 
 
-function DetailResult({navigation}) {
-    const base_url = 'https://dorar.net/dorar_api.json?skey='
+class DetailResult extends React.Component {
 
-    const [result, setResult] = useState(null)
-
-    async function fetchResult() {
-        const response = await fetch(base_url + navigation.getParam('q'))
-        const json = await response.json()
-        setResult(json)
-    }
-
-    useEffect(() => {fetchResult()}, [])
-
-    if (!result) {
-        return <View><Text>Loading...</Text></View>
-    }
-
-    function setData(response) {
-        const $ = cheerio.load(response.ahadith.result)
-        data = $('.hadith').map(function () {return $(this).text()}).get()
-        info = $('.hadith-info').map(function () {return $(this).text()}).get()
-        // console.log(data)
-        return _.zip(data, info)
-    }
-
-    async function onShare(hadith, info) {
-        // try {
-        //     const result = await Share.share({
-        //         message: hadith + '\n' + info,
-        //         dialogTitle: 'Share hadits',
-        //     })
-
-        //     alert(result.activityType)
-        // } catch (error) {
-        //     alert(error.message)
-        // }
-
-        try {
-            const result = await Share.open({
-                message: hadith + '\n' + info,
-                title: 'hello world'
-            })
-        } catch (error){
-            alert(error.message)
+    base_url = 'https://dorar.net/dorar_api.json?skey='
+    constructor(props){
+        super(props)
+        this.state = {
+            query: "",
+            result: null
         }
+
+        this.fetchResult = this.fetchResult.bind(this)
     }
 
-    function _renderItem({item}) {
-        return (<TouchableOpacity>
-            <View>
-                <Text style={{fontSize: 20, padding: 10, paddingBottom: 5, marginEnd: 5}}>
-                    {item[0]}
-                </Text>
-                <Text style={{fontSize: 15, paddingTop: 0, paddingEnd: 10}}>
-                    {item[1]}
-                </Text>
-
-            </View>
-            <Button onPress={e => {onShare(item[0], item[1])}} title={'Share'} />
-        </TouchableOpacity>
-        )
+    async fetchResult() {
+        const response = await fetch(this.base_url + this.props.navigation.getParam('q'))
+        const json = await response.json()
+        this.setState({result: json})
     }
 
-    return <ScrollView>
-        <FlatList
-            data={setData(result)}
-            renderItem={_renderItem}
-            keyExtractor={(item, index) => item + index}
-        />
-    </ScrollView>
+    componentWillMount(){
+        ShareMenu.getSharedText((text) => {
+            this.props.navigation.navigate('Detail', {q: text})
+        })
+    }
+
+    async componentDidMount(){
+        await this.fetchResult()
+    }
+
+    render(){
+        if (!this.state.result) {
+            return <View><Text>Loading...</Text></View>
+        }
+
+        function setData(response) {
+            const $ = cheerio.load(response.ahadith.result)
+            data = $('.hadith').map(function () {return $(this).text()}).get()
+            info = $('.hadith-info').map(function () {return $(this).text()}).get()
+            // console.log(data)
+            return _.zip(data, info)
+        }
+
+        async function onShare(hadith, info) {
+            // try {
+            //     const result = await Share.share({
+            //         message: hadith + '\n' + info,
+            //         dialogTitle: 'Share hadits',
+            //     })
+
+            //     alert(result.activityType)
+            // } catch (error) {
+            //     alert(error.message)
+            // }
+
+            try {
+                const result = await Share.open({
+                    message: hadith + '\n' + info,
+                    title: 'hello world',
+                    url: this.base_url + this.props.navigation.getParam('q')
+                })
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+
+        function _renderItem({item}) {
+            return (<TouchableOpacity>
+                <View>
+                    <Text style={{fontSize: 20, padding: 10, paddingBottom: 5, marginEnd: 5}}>
+                        {item[0]}
+                    </Text>
+                    <Text style={{fontSize: 15, paddingTop: 0, paddingEnd: 10}}>
+                        {item[1]}
+                    </Text>
+
+                </View>
+                <Button onPress={e => {onShare(item[0], item[1])}} title={'Share'} />
+            </TouchableOpacity>
+            )
+        }
+
+        return <ScrollView>
+            <FlatList
+                data={setData(this.state.result)}
+                renderItem={_renderItem}
+                keyExtractor={(item, index) => item + index}
+            />
+        </ScrollView>
+    }
+
+
 }
 
 DetailResult.navigationOptions = ({navigation}) => ({
