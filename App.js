af66e39ab3289e1adb9c4e66a9bf7a6415e6f5ba
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-08-16 23:34:00 +0700

    add some style

--- App.js
+++ App.js
@@ -41,31 +41,41 @@ class App extends React.Component {
         const {value} = this.state
         return (
             <View style={styles.container}>
-                <Text style={styles.headerText}>Dorar.net</Text>
-                <TextInput
-                    style={{height: 50}}
-                    placeholder="اكتب الحديث"
-                    value={value}
-                    onChangeText={(text) => {this.setState({value:text})}}
-                />
-                <Button onPress={() => navigation.navigate('Detail', {q: value})} title="بحث" />
+                <Text style={styles.headerText}>الدرر السنية</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={{height: 50, margin: 5}}
+                        placeholder="البحث في الموسوعة"
+                        value={value}
+                        onChangeText={(text) => {this.setState({value:text})}}
+                        onSubmitEditing={() => navigation.navigate('Detail', {q: value})}
+                        style={{fontSize:24}}
+                    />
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 20,
-  },
-  headerText: {
-    fontSize: 35,
-    fontWeight: "600"
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 20,
+        paddingBottom: 30,
+    },
+    headerText: {
+        fontSize: 35,
+        fontWeight: "600"
+    },
+    inputContainer: {
+        height: 50,
+        borderStyle: 'solid',
+        borderWidth: 0.75,
+        borderRadius: 25,
+    }
 });
 
 const DorarApp = createStackNavigator({
