4e2617bddbd8fccd13ae315535d11a3e0816999f
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-08-17 07:59:18 +0700

    add files

--- android/app/src/main/java/com/studio/MainApplication.java
+++ android/app/src/main/java/com/studio/MainApplication.java
@@ -3,6 +3,7 @@ package com.studio;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wenkesj.voice.VoicePackage;
 import com.meedan.ShareMenuPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VoicePackage(),
             new ShareMenuPackage(),
             new RNGestureHandlerPackage(),
             new ReanimatedPackage(),
